54936fc92ba00c99158cf67c84720be1e6f5650d
nbsteuv <nbsteuv@gmail.com>
2017-04-19 07:29:35 -0400

    Display list of customers in table

---
+++ CustomerInvoiceSystem/CustomerInvoiceSystem/Controllers/CustomersController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CustomerInvoiceSystem.Models;
+
+namespace CustomerInvoiceSystem.Controllers
+{
+    public class CustomersController : Controller
+    {
+        private ApplicationDbContext _context;
+
+        public CustomersController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
+        // GET: Customers
+        public ActionResult Index()
+        {
+            var customers = _context.Customers;
+            return View(customers);
+        }
+
+
+    }
+}
