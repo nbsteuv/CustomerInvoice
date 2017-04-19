2d78ab442474f148c01534d01fc2113c76eab361
nbsteuv <nbsteuv@gmail.com>
2017-04-19 07:44:40 -0400

    Added bootstrap sandstone theme

--- CustomerInvoiceSystem/CustomerInvoiceSystem/App_Start/BundleConfig.cs
+++ CustomerInvoiceSystem/CustomerInvoiceSystem/App_Start/BundleConfig.cs
@@ -24,7 +24,7 @@ namespace CustomerInvoiceSystem
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
+                      "~/Content/bootstrap-sandstone.css",
                       "~/Content/site.css"));
         }
     }
